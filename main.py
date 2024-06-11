54a8d58685e8dbecda9ba6a7d323ace2383a9fca
busypenguin <112860511+busypenguin@users.noreply.github.com>
2024-06-11 23:55:01 +0300

    save parsed books info in new foo  parse_book_page

--- main.py
+++ main.py
@@ -43,35 +43,51 @@ def download_image(url, filename, folder='images/'):
     return filepath
 
 
+def parse_book_page(num_book, response):
+    response.raise_for_status()
+    check_for_redirect(response)
+    book_info = {}
+    soup = BeautifulSoup(response.text, 'lxml')
+    book_and_author = soup.find('table').find(class_='ow_px_td').find('h1')
+    book_and_author_text = book_and_author.text
+    splited_book_and_author_text = book_and_author_text.split(' :: ')
+    book_name = str(num_book)+'. '+splited_book_and_author_text[0].strip()
+    book_info['Название'] = book_name
+    book_image = soup.find(class_='bookimage').find('img')['src']
+    imagepath = urljoin('https://tululu.org/', book_image)
+    parsed_url = urlparse(imagepath)
+    book_image_name = os.path.basename(parsed_url.path)
+    book_image = download_image(imagepath, book_image_name)
+    book_info['Картинка книги'] = book_image
+
+    comments = soup.find(id='content').find_all(class_='black')
+    comments_list = []
+    for comment in comments:
+        comment_text = comment.text
+        comments_list.append(comment_text)
+    book_info['Комментарии'] = comments_list
+
+    genres = soup.find(id='content').find('span', class_='d_book').find_all('a')
+    genres_list = []
+    for el in genres:
+        genre = el.text
+        genres_list.append(genre)
+    book_info['Жанр'] = genres_list
+    return book_info
+
+
 for num_book in range(1, 11):
     url_for_book_info = 'https://tululu.org/b{}/'.format(num_book)
     url_for_download_book = 'https://tululu.org/txt.php?id={}'.format(num_book)
     response_for_book_info = requests.get(url_for_book_info)
-    response_for_book_info.raise_for_status()
-
-    soup = BeautifulSoup(response_for_book_info.text, 'lxml')
-    book_and_author = soup.find('table').find(class_='ow_px_td').find('h1')
-
-    book_and_author_text = book_and_author.text
-    splited_book_and_author_text = book_and_author_text.split(' :: ')
-    book_name = str(num_book)+'. '+splited_book_and_author_text[0].strip()
 
     try:
-        filepath = download_txt(url_for_download_book, book_name)
-        book_image = soup.find(class_='bookimage').find('img')['src']
+        filepath = download_txt(url_for_download_book, parse_book_page(num_book, response_for_book_info)['Название'])
 
-        imagepath = urljoin('https://tululu.org/', book_image)
+        imagepath = urljoin('https://tululu.org/', parse_book_page(num_book, response_for_book_info)['Картинка книги'])
         parsed_url = urlparse(imagepath)
         book_image_name = os.path.basename(parsed_url.path)
-        image_ = download_image(imagepath, book_image_name)
-
-        comments = soup.find(id='content').find_all(class_='black')
-        for comment in comments:
-            comment_text = comment.text
-
-        genres = soup.find(id='content').find('span', class_='d_book').find_all('a')
-        for el in genres:
-            genre = el.text
+        book_image = download_image(imagepath, book_image_name)
 
     except requests.exceptions.HTTPError:
         continue
