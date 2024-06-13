03f4b1b97f542a5df041cc36b08918471f8ad080
busypenguin <112860511+busypenguin@users.noreply.github.com>
2024-06-13 17:56:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -44,37 +44,39 @@ def download_image(url, filename, folder='images/'):
     return filepath
 
 
-def parse_book_page(num_book, response):
+def parse_book_page(book_num, response):
     response.raise_for_status()
     check_for_redirect(response)
-    book_info = {}
     soup = BeautifulSoup(response.text, 'lxml')
     book_and_author = soup.find('table').find(class_='ow_px_td').find('h1')
     book_and_author_text = book_and_author.text
     splited_book_and_author_text = book_and_author_text.split(' :: ')
-    book_name = str(num_book)+'. '+splited_book_and_author_text[0].strip()
-    book_info['Название'] = book_name
-    book_image = soup.find(class_='bookimage').find('img')['src']
-    imagepath = urljoin('https://tululu.org/', book_image)
+    book_name = (f'{book_num}. {splited_book_and_author_text[0].strip()}')
+    book_image_path = soup.find(class_='bookimage').find('img')['src']
+    imagepath = urljoin('https://tululu.org/', book_image_path)
     parsed_url = urlparse(imagepath)
     book_image_name = os.path.basename(parsed_url.path)
-    book_image = download_image(imagepath, book_image_name)
-    book_info['Картинка книги'] = book_image
+    book_image_path = download_image(imagepath, book_image_name)
 
     comments = soup.find(id='content').find_all(class_='black')
-    comments_list = []
+    all_comments = []
     for comment in comments:
         comment_text = comment.text
-        comments_list.append(comment_text)
-    book_info['Комментарии'] = comments_list
+        all_comments.append(comment_text)
 
     genres = soup.find(id='content').find('span', class_='d_book').find_all('a')
-    genres_list = []
+    all_genres = []
     for el in genres:
         genre = el.text
-        genres_list.append(genre)
-    book_info['Жанр'] = genres_list
-    return book_info
+        all_genres.append(genre)
+    
+    book = {
+        'Название': book_name,
+        'Картинка книги': book_image_path,
+        'Комментарии': all_comments,
+        'Жанр': all_genres,
+    }
+    return book
 
 
 if __name__ == '__main__':
@@ -86,20 +88,19 @@ if __name__ == '__main__':
     args = parser.parse_args()
     start_id = args.start_id
     end_id = args.end_id
-    print(start_id, end_id)
 
-    for num_book in range(start_id, end_id+1):
-        url_for_book_info = 'https://tululu.org/b{}/'.format(num_book)
-        url_for_download_book = 'https://tululu.org/txt.php?id={}'.format(num_book)
-        response_for_book_info = requests.get(url_for_book_info)
+    for book_num in range(start_id, end_id+1):
+        url_for_book = 'https://tululu.org/b{}/'.format(book_num)
+        url_for_download_book = 'https://tululu.org/txt.php?id={}'.format(book_num)
+        response_for_book = requests.get(url_for_book)
 
         try:
-            filepath = download_txt(url_for_download_book, parse_book_page(num_book, response_for_book_info)['Название'])
+            filepath = download_txt(url_for_download_book, parse_book_page(book_num, response_for_book)['Название'])
 
-            imagepath = urljoin('https://tululu.org/', parse_book_page(num_book, response_for_book_info)['Картинка книги'])
+            imagepath = urljoin('https://tululu.org/', parse_book_page(book_num, response_for_book)['Картинка книги'])
             parsed_url = urlparse(imagepath)
             book_image_name = os.path.basename(parsed_url.path)
-            book_image = download_image(imagepath, book_image_name)
+            book_image_path = download_image(imagepath, book_image_name)
 
         except requests.exceptions.HTTPError:
             continue
